9d3c876e941fc8ed83df38fd7026df5701f89490
stephen <stephen@lumiterra.net>
2023-12-12 14:21:51 +0800

    init

---
+++ GameFramework/Network/INetworkChannelHelper.cs
@@ -0,0 +1,45 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+namespace GameFramework.Network
+{
+    /// <summary>
+    /// 网络频道辅助器接口。
+    /// </summary>
+    public interface INetworkChannelHelper
+    {
+        /// <summary>
+        /// 获取消息包头长度。
+        /// </summary>
+        int PacketHeaderLength
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 初始化网络频道辅助器。
+        /// </summary>
+        /// <param name="networkChannel">网络频道。</param>
+        void Initialize(INetworkChannel networkChannel);
+
+        /// <summary>
+        /// 关闭并清理网络频道辅助器。
+        /// </summary>
+        void Shutdown();
+
+        /// <summary>
+        /// 准备进行连接。
+        /// </summary>
+        void PrepareForConnecting();
+
+        /// <summary>
+        /// 发送心跳消息包。
+        /// </summary>
+        /// <returns>是否发送心跳消息包成功。</returns>
+        bool SendHeartBeat();
+    }
+}
